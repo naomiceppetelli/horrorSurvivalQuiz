78f3cbc9bdff163e1ee9edb07753e89506562ffc
Naomi Ceppetelli <naomiceppetelli@gmail.com>
2018-05-06 12:51:37 -0400

    fixed submit button double

--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,28 @@ const app = {};
 
 app.answers = {
     very: {
-        result: "very likely to survive",
+        result: "Very likely to survive!",
+        twitterResult: "I am very likely to survive a horror movie scenario!",
         description: "Congratulations! You've survived the life shattering experience of being the sole survivor in a horror movie.  Enjoy your years of therapy!"
     },
     somewhat: {
-        result: "somewhat likely to suvive",
+        result: "Somewhat likely to suvive!",
+        twitterResult: "I might survive a horror movie scenario!",
         description: "You almost made it! You saw the second half of the movie at least. You might have died trying to be the hero or in some twist of dramatic irony, either way your luck eventually ran out"
     },
     notVery: {
-        result: "not very likely to survive",
+        result: "Not very likely to survive!",
+        twitterResult:"I would likely not survive a horror movie scenario",
         description: "You weren't the first to die! But you also weren't far behind. You were probably moved by the disturbing death of your friend right before you and it clouded your judgement. Try to be a little more callous next time if you want to live"
     },
     unlikely: {
-        result: "super unlikely to survive",
+        result: "Super unlikely to survive!",
+        twitterResult: "It is super unlikely that I would survive a horror movie scenario",
         description: "You were the first to die! We're not sure you were even trying honestly. Take a long hard look at your choices and maybe take the quiz again"
     },
     killer: {
-        result: "extremely likely to survive...because you are the killer",
+        result: "Extremely likely to survive...because you are the killer!!",
+        twitterResult: "I would survive a horror movie because I'd likely be the killer!",
         description: "Plot twist! You're sick enough to be the killer, congratulations!! Go home, sharpen your knives, and wait for another group of teens to wander near your cabin in the woods"
     }
 };
@@ -38,7 +43,6 @@ app.checkScore = function(array) {
         // console.log(array[i])
         app.tally[array[i]] = app.tally[array[i]] + 1;
     }
-    console.log(app.tally);
     app.largestNumber();
     
 }
@@ -46,25 +50,41 @@ app.checkScore = function(array) {
 //this turns the newly tallied app.tally into an array 
 app.largestNumber = function() {
     const tallyEntries = Object.entries(app.tally);
-    console.log(tallyEntries);
     tallyEntries.sort((a,b) => {
         return b[1] - a[1]
     });
     const final = tallyEntries[0][0];
-    console.log(final)
     app.displayAnswer(final);
 }
 
 app.displayAnswer = (answer) => {
+    console.log('called twice')
     const finalResult = app.answers[answer].result;
     const finalResultDescription = app.answers[answer].description;
+    const finalTwitterBlurb = app.answers[answer].twitterResult;
     console.log(`${finalResult}, ${finalResultDescription}`)
     //print on page 
-    app.printResultsOnPage(finalResult, finalResultDescription);
+    $('.question5').hide();
+    $('.results').empty()
+        .css('display', 'flex')
+        .append(`
+        <h2>${finalResult}</h2>
+        <h3>${finalResultDescription}</h3>
+        <button class="retake">Take the quiz again!</button>
+        <div class="social">
+            <p>Share Your Results!</p>
+            <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=%20${finalTwitterBlurb} %20Discover%20your%20odds%20at%20http://naomiceppetelli.com/horrorMovieHoedown"
+            ><i class="fab fa-twitter"></i></a>
+        </div>
+        `)
+        .show();
+    $('.retake').on('click', function () {
+        location.reload(true);
+    }); 
 }
 
-app.events = function() {
-    $('input[type=submit]').on('click', function(e){
+app.events = function (result, twitterBlurb, description) {
+    $('input[type=submit]').on('click', function (e){
         //prevent the page from refreshing
         e.preventDefault();
         //want to save each answer as a variable 
@@ -76,8 +96,9 @@ app.events = function() {
         //putting answers into an array so that can work with the data 
         const answerArray = [answer1, answer2, answer3, answer4, answer5]
         //enter answerArray into the tallying function
-        app.checkScore(answerArray);  
+        app.checkScore(answerArray);
     });
+
 
     //button functionality 
     $('.takeQuiz').on('click', function(){
@@ -145,26 +166,6 @@ app.events = function() {
         $('.question4').fadeIn()
     })
 }
-
-//results funcitonality 
-app.printResultsOnPage = function(result, description) {
-    $('input[type=submit]').on('click', function(){
-        $('.question5').hide();
-        $('.results').empty();
-        $('.results').css('display:flex');
-        $('.results').show();
-        $('.results').append(`
-        <h2>${result}</h2>
-        <h2>${description}</h2>
-        <button class="retake">Take the quiz again!</button>
-        `);
-        $('.retake').on('click', function(){
-            location.reload(true);
-        });
-    });
-};
-
-
 
 app.init = function(){
     app.events();
